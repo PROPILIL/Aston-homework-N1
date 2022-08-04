a8e7ffb7f351bbefd89c9d34725be6c300050211
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-08-05 01:59:37 +0300

    It's working!!!

--- app/src/main/java/com/propil/homework1_2/MainActivity.kt
+++ app/src/main/java/com/propil/homework1_2/MainActivity.kt
@@ -2,10 +2,51 @@ package com.propil.homework1_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    var counter = 1
+    var rollFlag = false
+
+    private lateinit var textViewHint: TextView
+    private lateinit var diceSide: TextView
+    private lateinit var rollButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewHint = findViewById(R.id.textview)
+        diceSide = findViewById(R.id.dice_side)
+        rollButton = findViewById(R.id.roll_button)
+
+
+        rollButton.setOnClickListener() {
+            when (rollFlag) {
+                false -> {
+                    rollButton.text = getString(R.string.roll_button_stop)
+                    rollFlag = true
+                    GlobalScope.launch {
+                        while (rollFlag) {
+                            counter++
+                            if (counter == 7) counter = 1
+                            GlobalScope.launch(Dispatchers.IO) {
+                                diceSide.post { diceSide.text = "$counter" }
+                            }
+                        }
+                        delay(500L)
+                    }
+                }
+                true -> {
+                    rollButton.text = getString(R.string.roll_button_start)
+                    rollFlag = false
+                }
+            }
+
+        }
     }
 }
